724784355aa80812046f24b7d57f4096b8626b76
John Ndungu <jndungu@STLMSD.COM>
2018-10-31 10:11:55 -0500

    catch up branches

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,48 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { BookComponent } from './book/book.component';
+import { MixesComponent } from './mixes/mixes.component';
+import { LibraryComponent } from './library/library.component';
+import { MenuComponent } from './menu/menu.component';
+import { UpcomingShowsComponent } from './upcoming-shows/upcoming-shows.component';
+import { HomeComponent } from './home/home.component';
+
+
+const appRoutes : Routes = [
+  // {path: '', component: AppComponent},
+  {path: '', component: HomeComponent},
+  {path: 'book', component: BookComponent}, 
+  {path: 'mixes', component: MixesComponent},
+  {path: 'library', component: LibraryComponent},
+  {path: 'upcoming-shows', component: UpcomingShowsComponent},
+  { path: '**', component: HomeComponent } 
+] 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
-  ],
+    FooterComponent,
+    BookComponent,
+    MixesComponent,
+    LibraryComponent,
+    MenuComponent,
+    UpcomingShowsComponent,
+    HomeComponent
+  ], 
+
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } //TODO remove when done
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
