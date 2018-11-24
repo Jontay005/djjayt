4ef0c33a52f84e76a05f07d4a6985ba14378fa3c
jndungu <johnwakaba@gmail.com>
2018-11-24 12:00:46 -0600

    edited upcoming shows page

--- src/app/upcoming-shows/upcoming-shows.component.ts
+++ src/app/upcoming-shows/upcoming-shows.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./upcoming-shows.component.css']
 })
 export class UpcomingShowsComponent implements OnInit {
-
+  title: string;
+  shows: any[];
+  
   constructor() { }
 
   ngOnInit() {
+    this.title = 'Upcoming Shows December 2018';
+    this.shows = [
+        { id: 1, date: '12/1', Location: 'Phoenix, Arizona.', venue: 'Jambo Bar'},
+        { id: 2, date: '12/2', Location: 'Dallas, Texas.', venue: 'Kaldis Coffee'},
+        { id: 3, date: '12/3', Location: 'Seattle, Washington.', venue: 'Olive Bar'},
+        { id: 4, date: '12/4', Location: 'New York, New York.', venue: 'Rafikis'}
+    ];
   }
-
 }
